85b5803ec1a8ea35990b9b1cdf7c978feed3dafe
Muhammad Shahbaz <MuhammadShahbazali001@gmail.com>
2026-07-06 10:34:33 +0500

    Error handling and Custom Error and async and sync programming and call backs

--- index.js
+++ index.js
@@ -1,59 +1,162 @@
-console.log("Hello World")
+// console.log("Hello World")
 
-//===================== COWSAY =========================
+// //===================== COWSAY =========================
 
-const cowsay = require('cowsay');
+// const cowsay = require('cowsay');
 
-console.log(
-    cowsay.say({
-        text :"Node Js Class 2",
-        e:"**",
-        T:"U"
-    })
-)
+// console.log(
+//     cowsay.say({
+//         text: "Node Js Class 2",
+//         e: "**",
+//         T: "U"
+//     })
+// )
 
-//=================== Chalk =============================
+// //=================== Chalk =============================
 
-const chalk = require('chalk')
+// const chalk = require('chalk')
 
-console.log(chalk.green('hello'))
-console.log(chalk.red('World'))
-console.log(chalk.bgBlue('World'))
+// console.log(chalk.green('hello'))
+// console.log(chalk.red('World'))
+// console.log(chalk.bgBlue('World'))
 
-console.log(
-    chalk.red(
-        cowsay.say({
-            text : ' ',
-            e:"0o",
-            T:""
-        })
-    )
-)
+// console.log(
+//     chalk.red(
+//         cowsay.say({
+//             text: ' ',
+//             e: "0o",
+//             T: ""
+//         })
+//     )
+// )
 
-//========================== DOTENV ==========================
+// //========================== DOTENV ==========================
 
-const dontenv = require("dotenv")
-dontenv.config();
-//====================== READING ENV VARIABLES ==============
+// const dontenv = require("dotenv")
+// dontenv.config();
+// //====================== READING ENV VARIABLES ==============
 
-console.log(process.env.PORT)
-console.log(process.env.DB_USERNAME)
-console.log(process.env.DB_PASS)
+// console.log(process.env.PORT)
+// console.log(process.env.DB_USERNAME)
+// console.log(process.env.DB_PASS)
 
-//===================== IMPORT EXPORT MODULES IN NODEJS ==========
+// //===================== IMPORT EXPORT MODULES IN NODEJS ==========
 
-const car = require('./car.js')
-console.log(car)
+// const car = require('./car.js')
+// console.log(car)
 
-const myMath = require('./myMaths.js')
+// const myMath = require('./myMaths.js')
 
-console.log(myMath.sum(4,5))
+// console.log(myMath.sum(4, 5))
 
-const {sum , sub , mul , div , PI} = require('./myMaths.js')
+// const { sum, sub, mul, div, PI } = require('./myMaths.js')
 
-sum(4,4)
-sub(8,4)
-mul(8,4)
-div(8,4)
+// sum(4, 4)
+// sub(8, 4)
+// mul(8, 4)
+// div(8, 4)
 
-console.log(PI)
+// console.log(PI)
+
+//=========================Exception Handling Error Handling ==============================
+
+const greetings = (name) => {
+    console.log(`Hello ${name.toUpperCase()}`)
+}
+
+// greetings('shahbaz');
+
+
+// greetings(12);
+
+console.log("Before Error")
+
+try {
+    // greetings('shahbaz');
+    greetings(12);
+
+} catch (error) {
+    console.log(error.message)
+} finally {
+    console.log('I am finally')
+}
+console.log("After Error")
+
+//======================== THROWING CUSTOM ERROR ===============================
+
+const divide = (x, y) => {
+    if (typeof x != "number" && typeof y != "number") {
+        throw new Error("Input Must be in Numbers")
+    }
+
+    if (y === 0) {
+        throw new Error("Cannot Divide By Zero")
+    }
+
+    console.log(x / y)
+}
+
+
+// divide("a",5)
+// divide("abc","abv")
+
+try {
+    // divide(4,0)
+    divide("a", "a")
+} catch (error) {
+    console.log(error.message)
+}
+
+//==================unCaught Exception======================
+
+process.on("uncaughtException", () => {
+    console.log("An uncaught Exception Occured")
+    process.exit()
+})
+
+// divide(10 , 0)
+
+
+//==================SYnchronous Programming =============
+
+console.log("A")
+console.log("B")
+console.log("C")
+console.log("D")
+
+// const syncFunction = () =>{
+//     let futureTime = new Date().getTime() + 3000
+//     while(futureTime > new Date().getTime()){
+//         console.log("processing ....")
+//     }
+// }
+
+// console.log("Line # 1 ")
+// syncFunction();
+// console.log("Line # 2 ")
+
+//========================= ASYNCHRONOUS PROGRAMMING ===============
+
+const asyncFunction = () => {
+    setTimeout(() => {
+        console.log("Processing .....")
+    }, 3000) 
+}
+
+console.log("Line # 1 ")
+asyncFunction();
+console.log("Line # 2 ")
+
+//=================== Call Backs =========================
+
+const ConnectToDb = (cb) => {
+    console.log("Starting to Connect To Database")
+    console.log("😴😴😴😴😴😴😴")
+    console.log("Connected To Database")
+    cb();
+}
+
+ConnectToDb(()=>{
+    console.log("I AM CB FUNCTION")
+})
+
